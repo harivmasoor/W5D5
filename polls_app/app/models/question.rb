d88c86913bdd3cfdfcaea033ea08e687c5bab8cd
harivmasoor <harimasoor@gmail.com>
2023-06-25 18:20:59 -0700

    finish

--- polls_app/app/models/question.rb
+++ polls_app/app/models/question.rb
@@ -1,6 +1,15 @@
 class Question < ApplicationRecord
     belongs_to :poll
-    has_many :answer_choices, dependent: :destroy
-
-    validates :text, presence: true
+    has_many :answer_choices
+    has_many :responses, through: :answer_choices
+  
+    def results
+      answer_choices
+        .select("answer_choices.*, COUNT(responses.id) AS responses_count")
+        .left_joins(:responses)
+        .group("answer_choices.id")
+        .order("responses_count DESC")
+        .pluck(:text, "COUNT(responses.id)")
+        .to_h
+    end
   end
